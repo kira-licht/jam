45ed3a38133fa579be7bba56da688298c5a7d28c
kira-licht <159264905+kira-licht@users.noreply.github.com>
2024-02-09 19:47:25 +0800

    Add files via upload

---
+++ sam.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you too! 😘";
+  gif.src = "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
+});
+
+noBtn.addEventListener("click", () => {
+  const maxX = window.innerWidth - noBtn.clientWidth;
+  const maxY = window.innerHeight - noBtn.clientHeight;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.transform = `translate(${randomX}px, ${randomY}px)`;
+});
